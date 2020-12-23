1c42b041c81c92d8c66fc9bcd2d01933cabfcd36
Dincer Aslan <dinceraslan.com@gmail.com>
2020-12-23 21:17:54 +0300

    Updated output.log file path

--- lawgokr/logger.py
+++ lawgokr/logger.py
@@ -3,5 +3,6 @@ import logging
 
 def message(message):
     logging.warning(message)
-    with open('../output.log', 'a') as file:
+    with open('output.log', 'a') as file:
+
         file.write(message + '\n')
